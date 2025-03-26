0255d0790c2347c0f617bf9c184052d28ae22ca1
rohan <rohanprasad1434@gmail.com>
2025-03-26 10:45:42 +0530

    new

--- AreaPostOffice.java
+++ AreaPostOffice.java
@@ -1,7 +1,7 @@
-class AreaPostOffice{
-	public static void deliver(String customername,String address)
-	{
-	System.out.println("AreaPostOffice:"+customername+"Deliver:"+address);
-	SubRegionalPostOffice.deliver(customername,address);
+class AreaPostOffice {
+	public static void deliver(String customername, String address) {
+		System.out.println("AreaPostOffice:" + customername + "Deliver:" + address);
+		SubRegionalPostOffice.deliver(customername, address);
 	}
-	}
+
+}
